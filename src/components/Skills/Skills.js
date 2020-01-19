7e7efdcbb117ec31a6309b6b376d6db6374a90d0
Ahmet Topic <ahmettopic@hotmail.com>
2020-01-20 00:13:29 +0100

    added JSON data

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+// JSON Data
+import data from "../../data/data.json";
 
 import "./Skills.css";
 
-const skills = props => (
-  <React.Fragment>
-    <div className="Skills">
-      <p>Skills</p>
-    </div>
-  </React.Fragment>
-);
+class Skills extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="Skills">
+          <h1>Skills</h1>
+          {data.map((skillDetail, index) => {
+            return (
+              <div className="Title">
+                <p>{skillDetail.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
-export default skills;
+export default Skills;
