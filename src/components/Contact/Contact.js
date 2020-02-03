592256fc56269733acf1110f88c82cfb7df04699
Ahmet Topic <ahmettopic@hotmail.com>
2020-02-03 17:49:42 +0100

    update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,17 @@
 import React from "react";
+// Material Grid
+import Grid from "@material-ui/core/Grid";
 // CSS
 import "../../styles/styles.css";
 
 const contact = props => (
   <React.Fragment>
     <div className="Contact">
-      <p>Contact</p>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item>
+          <p>Contact</p>
+        </Grid>
+      </Grid>
     </div>
   </React.Fragment>
 );
