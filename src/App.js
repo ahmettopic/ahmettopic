ac71f9fd26b08cb297426344f2390ee06efdd5e9
Ahmet Topic <ahmettopic@hotmail.com>
2020-02-03 02:04:18 +0100

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component, lazy, Suspense } from "react";
 // Components
 import Welcome from "./components/Welcome/Welcome";
+// Loader
+import { Block } from "styled-loaders-react";
 // LazyLoad
 const Layout = lazy(() => import("./Layout/Layout"));
 
 class App extends Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        loading: false
+      });
+    }, 5000);
+  };
+
   render() {
     return (
       <React.Fragment>
         <Welcome name="PORTFOLIO">
           Ahmet Topic / Fullstack Web Developer
         </Welcome>
-        <Suspense fallback={<p>SPINNER</p>}>
-          <Layout />
-        </Suspense>
+        <Suspense
+          fallback={this.state.loading ? <Spinner /> : <Layout />}
+        ></Suspense>
       </React.Fragment>
     );
   }
