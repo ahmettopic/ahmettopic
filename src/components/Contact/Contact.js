082382ca8bea900a784eb16745c4456010c80660
Ahmet Topic <ahmettopic@hotmail.com>
2020-02-14 01:22:01 +0100

    Auth base

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,29 @@
-import React from "react";
+import React from 'react';
 // Material Grid
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 // Styled Components
-import StyledParagraph from "../../styles/StyledParagraph";
+import StyledParagraph from '../../styles/StyledParagraph';
+// Components
+import Auth from '../../containers/Auth/Auth';
 
 const contact = props => (
   <div
     style={{
-      padding: "1em",
-      display: "flex",
-      alignItems: "center",
-      textAlign: "center",
-      justifyContent: "center"
+      padding: '1em',
+      display: 'flex',
+      alignItems: 'center',
+      textAlign: 'center',
+      justifyContent: 'center'
     }}
   >
     <Grid container justify="center">
       <Grid item xs={12}>
-        <StyledParagraph style={{ textAlign: "center" }}>
-          <b>Some content on this page is still being developed</b>. Meanwhile
-          visit
-          <a
-            style={{
-              margin: "4px",
-              textDecoration: "underline",
-              color: "black"
-            }}
-            target="_blank"
-            href="https://se.linkedin.com/in/ahmettopic?trk=profile-badge"
-            rel="noreferrer noopener"
-          >
-            LinkedIn
-          </a>
+        <StyledParagraph style={{ textAlign: 'center' }}>
+          Please Login or Sign Up to continue!
         </StyledParagraph>
-        <StyledParagraph style={{ textAlign: "center", marginTop: "1em" }}>
-          ahmettopic@hotmail.com
+        <Auth />
+        <StyledParagraph style={{ textAlign: 'center' }}>
+          By signing up, you accept the Privacy Policy
         </StyledParagraph>
       </Grid>
     </Grid>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+// Components
+import Input from '../../components/Layout/Input/Input';
+import Button from '../../components/Layout/Button/Button';
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Email'
+        },
+        value: '',
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    const form = formElementsArray.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.value}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
+    return (
+      <div>
+        {form}
+        <form action="">
+          <Button>SUBMIT</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
